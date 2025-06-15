9ccd4ef32fc7d3f26534a2b85c83f631ae7be4c5
Carlos-Zullo <chzullo17@gmail.com>
2025-06-15 18:35:53 -0300

    Create Main.java

---
+++ ed04/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Classe principal para iniciar o sistema de Agenda de Contatos.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ContatoView view = new ContatoView();
+        view.exibirMenu();
+    }
+}
