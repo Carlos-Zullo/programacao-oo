64b95d47b6ad601d8b7edd14be4f2bf556c5dc84
Carlos-Zullo <chzullo17@gmail.com>
2026-03-29 21:24:25 -0300

    Delete ed04 directory

--- ed04/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Classe principal para iniciar o sistema de Agenda de Contatos.
- */
-public class Main {
-    public static void main(String[] args) {
-        ContatoView view = new ContatoView();
-        view.exibirMenu();
-    }
-}
--- ed04/model/Contato.java
+++
@@ -1,8 +0,0 @@
-/**
- * Interface para definição de métodos básicos de um contato.
- */
-public interface Contato {
-    String getNome();
-    String getTelefone();
-    String getEmail();
-}
