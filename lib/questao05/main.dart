5a63d1844a70d11255bc8c76ac7ff544d9f18e57
Carlos-Zullo <chzullo17@gmail.com>
2026-03-29 22:25:21 -0300

    Add main.dart to display student reports

---
+++ lib/questao05/main.dart
@@ -0,0 +1,25 @@
+import 'aluno.dart';
+ 
+void main() {
+  Aluno a1  = Aluno('Ana',     9.0, 8.5);
+  Aluno a2  = Aluno('Bruno',   5.0, 4.0);
+  Aluno a3  = Aluno('Carlos',  7.0, 6.5);
+  Aluno a4  = Aluno('Diana',   3.0, 2.5);
+  Aluno a5  = Aluno('Eduardo', 10.0, 9.0);
+  Aluno a6  = Aluno('Fernanda',6.0, 5.5);
+  Aluno a7  = Aluno('Gabriel', 4.0, 3.5);
+  Aluno a8  = Aluno('Helena',  8.0, 7.0);
+  Aluno a9  = Aluno('Igor',    1.0, 2.0);
+  Aluno a10 = Aluno('Julia',   6.5, 7.5);
+ 
+  a1.exibirBoletim();
+  a2.exibirBoletim();
+  a3.exibirBoletim();
+  a4.exibirBoletim();
+  a5.exibirBoletim();
+  a6.exibirBoletim();
+  a7.exibirBoletim();
+  a8.exibirBoletim();
+  a9.exibirBoletim();
+  a10.exibirBoletim();
+}
