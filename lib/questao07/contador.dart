926e77fc0ad57c64f49cba6c75d14e23d3356e9b
Carlos-Zullo <chzullo17@gmail.com>
2026-03-29 22:27:40 -0300

    Implement Contador class with increment/decrement methods

---
+++ lib/questao07/contador.dart
@@ -0,0 +1,34 @@
+class Contador {
+  int valor;
+ 
+  Contador(this.valor) {
+    if (valor < 0) {
+      throw ArgumentError('O valor inicial deve ser maior ou igual a 0.');
+    }
+  }
+ 
+  void incrementar() {
+    valor++;
+    print('Incrementado. Valor atual: $valor');
+  }
+ 
+  void decrementar() {
+    if (valor == 0) {
+      print('O valor já é 0, não pode ficar negativo.');
+      return;
+    }
+    valor--;
+    print('Decrementado. Valor atual: $valor');
+  }
+ 
+  void zerar() {
+    valor = 0;
+    print('Contador zerado.');
+  }
+ 
+  void exibirValor() {
+    print('-------------------------');
+    print('Valor atual: $valor');
+    print('-------------------------');
+  }
+}
