069596df2bc8e1dc700b5d94664fb39a8cec066c
Carlos-Zullo <chzullo17@gmail.com>
2026-03-29 22:39:49 -0300

    Implement main function to display product summary

---
+++ lib/questao16/main.dart
@@ -0,0 +1,13 @@
+import 'produto.dart';
+
+void main() {
+  Produto p = Produto(1, 'Notebook', 3500.00, 10);
+  p.exibirResumo();
+
+  // Substituindo os atributos
+  p.codigo = 2;
+  p.nome = 'Mouse Gamer';
+  p.precoUnitario = 150.00;
+  p.quantidade = 20;
+  p.exibirResumo();
+}
