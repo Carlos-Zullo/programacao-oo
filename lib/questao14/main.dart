66e39285e69e939cd15ac05a7db174df9df8db9f
Carlos-Zullo <chzullo17@gmail.com>
2026-03-29 22:37:52 -0300

    Add main function to control Lampada state
    Implement main function to demonstrate Lampada usage.

---
+++ lib/questao14/main.dart
@@ -0,0 +1,18 @@
+import 'lampada.dart';
+
+void main() {
+  Lampada l = Lampada();
+
+  l.exibirEstado();
+  l.ligar();
+  l.desligar();
+  l.ligar();
+  l.desligar();
+  l.ligar();
+  l.desligar();
+  l.ligar();
+  l.desligar();
+  l.ligar(); // 5ª ligação — queima aqui
+  l.exibirEstado();
+  l.ligar(); // tentativa após queimar
+}
