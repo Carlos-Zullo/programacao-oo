29233f0d81f2097dec519937734d56e6f3c78ab7
Carlos-Zullo <chzullo17@gmail.com>
2026-03-29 22:17:10 -0300

    Add main.dart to display date objects

---
+++ lib/questao03/main.dart
@@ -0,0 +1,11 @@
+import 'data.dart';
+ 
+void main() {
+  Data d1 = Data(1, 1, 2000);
+  Data d2 = Data(15, 6, 1995);
+  Data d3 = Data(29, 3, 2026);
+ 
+  d1.exibirData();
+  d2.exibirData();
+  d3.exibirData();
+}
