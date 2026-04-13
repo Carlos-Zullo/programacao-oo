f7219f710ad02d12a741ae69cb970ca19a75e8d0
Carlos-Zullo <chzullo17@gmail.com>
2026-04-12 22:29:45 -0300

    Create lista-carrinho.dart

---
+++ estudo-dirigido-1.3/lib/questao06/lista-carrinho.dart
@@ -0,0 +1,39 @@
+import '../questao05/item_carrinho.dart';
+
+class ListaCarrinho {
+  final List<ItemCarrinho> _itens = [];
+
+  void inserir(ItemCarrinho item) {
+    _itens.add(item);
+    print('Item ${item.produto.nome} inserido no carrinho.');
+  }
+
+  void remover(int i) {
+    if (i < 0 || i >= _itens.length) {
+      print('Índice inválido para remover.');
+      return;
+    }
+    print('Item ${_itens[i].produto.nome} removido do carrinho.');
+    _itens.removeAt(i);
+  }
+
+  int tamanho() => _itens.length;
+
+  ItemCarrinho? get(int i) {
+    if (i < 0 || i >= _itens.length) {
+      print('Índice inválido.');
+      return null;
+    }
+    return _itens[i];
+  }
+
+  void exibirLista() {
+    if (_itens.isEmpty) {
+      print('Carrinho vazio.');
+      return;
+    }
+    for (var item in _itens) {
+      item.exibirItem();
+    }
+  }
+}
