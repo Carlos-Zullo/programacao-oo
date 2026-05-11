69c8f056dd0bc5ea83bb425b8897ffdb4cf159ea
Carlos-Zullo <chzullo17@gmail.com>
2026-05-10 21:11:17 -0300

    Create conta.dart

---
+++ estudo-dirigido-2.1/lib/questao04/conta.dart
@@ -0,0 +1,48 @@
+class ContaBancaria {
+  String titular;
+  double saldo;
+
+  ContaBancaria(this.titular, this.saldo);
+
+  void depositar(double valor) {
+    if (valor <= 0) { print('Valor inválido.'); return; }
+    saldo += valor;
+    print('Depósito de R\$ ${valor.toStringAsFixed(2)} realizado.');
+  }
+
+  void sacar(double valor) {
+    if (valor <= 0) { print('Valor inválido.'); return; }
+    if (valor > saldo) { print('Saldo insuficiente.'); return; }
+    saldo -= valor;
+    print('Saque de R\$ ${valor.toStringAsFixed(2)} realizado.');
+  }
+
+  void exibirSaldo() {
+    print('-------------------------');
+    print('Titular: $titular');
+    print('Saldo:   R\$ ${saldo.toStringAsFixed(2)}');
+    print('-------------------------');
+  }
+}
+
+class ContaPoupanca extends ContaBancaria {
+  double taxaRendimento = 0.01;
+
+  ContaPoupanca(String titular, double saldo) : super(titular, saldo);
+
+  void aplicarRendimento() {
+    saldo += saldo * taxaRendimento;
+    print('Rendimento de 1% aplicado.');
+  }
+}
+
+class ContaCorrente extends ContaBancaria {
+  double taxaRendimento = 0.05;
+
+  ContaCorrente(String titular, double saldo) : super(titular, saldo);
+
+  void aplicarRendimento() {
+    saldo += saldo * taxaRendimento;
+    print('Rendimento de 5% aplicado.');
+  }
+}
