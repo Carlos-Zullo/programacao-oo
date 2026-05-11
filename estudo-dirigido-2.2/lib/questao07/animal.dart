9f32bbc0a17e73f159c9c9a6eea4f6851dee59d1
Carlos-Zullo <chzullo17@gmail.com>
2026-05-10 21:21:49 -0300

    Create animal.dart

---
+++ estudo-dirigido-2.2/lib/questao07/animal.dart
@@ -0,0 +1,35 @@
+abstract class Animal {
+  String nome;
+
+  Animal(this.nome);
+
+  void emitirSom(); // método abstrato
+}
+
+class Cachorro extends Animal {
+  Cachorro(String nome) : super(nome);
+
+  @override
+  void emitirSom() => print('$nome diz: Au au! 🐶');
+}
+
+class Gato extends Animal {
+  Gato(String nome) : super(nome);
+
+  @override
+  void emitirSom() => print('$nome diz: Miau! 🐱');
+}
+
+class Passaro extends Animal {
+  Passaro(String nome) : super(nome);
+
+  @override
+  void emitirSom() => print('$nome diz: Piu piu! 🐦');
+}
+
+class Vaca extends Animal {
+  Vaca(String nome) : super(nome);
+
+  @override
+  void emitirSom() => print('$nome diz: Muuu! 🐄');
+}
